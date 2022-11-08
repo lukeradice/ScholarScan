f5b41ef7f6b506eaa408fa3c6de61b0741edfd96
Luke Radice <radiceluke@gmail.com>
2022-11-08 20:20:18 +0000

    Made html templates and allowed them to be accessed with views

---
+++ main.py
@@ -0,0 +1,8 @@
+from website import create_app
+
+app = create_app()
+
+#this condition is met if we run this file
+if __name__ == "__main__":
+    #runs flask application, stratup a web server 
+    app.run(debug=True)
--- website/views.py
+++ website/views.py
@@ -1,1 +1,13 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+#Blueprint allows us to define the URLs across multiple files
+#redner template allows us to render externally written html templates
+
+views = Blueprint("views", __name__)
+
+@views.route("/", methods=["POST", "GET"])
+def scholarScan():
+    return render_template("main.html")
+
+@views.route("/about", methods=["POST", "GET"])
+def about():
+    return render_template("about.html")
