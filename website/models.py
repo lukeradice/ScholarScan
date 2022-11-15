a16b8231abdc3f81ec4eab5e18e2aa7919f3b4c6
Luke Radice <radiceluke@gmail.com>
2022-11-15 19:15:01 +0000

    lot done today, overhaul of database, created more entities to make it  more normalised, attempting my first processing of attributes before  submission to the database, having a problem with checking a string  with a value in the database but I didn't look up documentation for it  I'm sure I'll be able to fix it easily next time

--- website/models.py
+++ website/models.py
@@ -5,9 +5,39 @@ class Study(db.Model):
     #creating study entity to store study data in once retrieved from a search
     id = db.Column(db.Integer, primary_key=True)
     searchDepth = db.Column(db.Integer)
-    peerReviewed = db.Column(db.Boolean)
     governmentAffiliation = db.Column(db.Boolean)
-    governmentReputation = db.Column(db.String)
+    title = db.Column(db.String(50))
+    abstract = db.Column(db.String(2000))
+    government_id = db.Column(db.Integer, db.ForeignKey('government.id'))
+    
+class Author(db.Model):
+    #the same author will have multiple studies so a separate table must be made for them, also a useful place to store other 
+    #information about them to determine their reputation
+    id = db.Column(db.Integer, primary_key=True)
+    authorName = db.Column(db.String(50))
     authorStudyCount = db.Column(db.Integer)
+    journalOrganisation_id = db.Column(db.Integer, db.ForeignKey('journalorganisation.id'))
+
+
+class AuthorStudyLink(db.Model):
+    #link table for Study and Author entities as they have a many to many relationship
+    #composite primary key of both the foreign keys and an id
+    id = db.Column(db.Integer, primary_key=True)
+    author_id = db.Column(db.Integer, db.ForeignKey('author.id'), primary_key=True)
+    study_id = db.Column(db.Integer, db.ForeignKey('study.id'), primary_key = True)
+
+class JournalOrganisation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    peerReviewed = db.Column(db.Boolean)
+    impactFactor = db.Column(db.Integer)
+
+class Government(db.Model):
+    #the same goverment will have multiple studies so a separate table must be made for them, also a useful place to store other 
+    #information about them to determine their reputation
+    id = db.Column(db.Integer, primary_key=True)
+    government = db.Column(db.String(20))
+    governmentReputation = db.Column(db.Integer)
+    correspondingDomain = db.Column(db.String(5))
+    
 
 
--- website/search.py
+++ website/search.py
@@ -1,5 +1,8 @@
 from scholarly import scholarly, ProxyGenerator
 from flask import flash
+from . import db
+from .models import Study, Author, AuthorStudyLink, Government
+import tldextract
 
 def search(searchQuery, peerReviewed, governmentAffiliation, overNStudies, resultAmount):
     print(searchQuery)
@@ -15,11 +18,38 @@ def search(searchQuery, peerReviewed, governmentAffiliation, overNStudies, resul
     if success:
         print("success")
         studies = scholarly.search_pubs(str(searchQuery))
+        count = 0
         for i in range (0, searchIterations):
             study = next(studies)
             if study:
-                abstract = study.get('bib')
-                print(abstract)
+                abstract = study.get('bib').get('abstract')
+                title = study.get('bib').get('title')
+                governmentAffiliation = attributeHandler.governmentAffiliation(study.get('pub_url'))
+                levelOfAffiliation = governmentAffiliation.levelOfAffiliation
+                government_id = governmentAffiliation.government_id
+                new_study = Study(SearchDepth=searchIterations, governmentAffiliation=levelOfAffiliation, title=title, abstract=abstract, government_id=government_id)
+                db.session.add(new_study)
+                db.session.commit()
             i = i + 1
-    
-    
+
+class attributeHandler():
+    def __init__(self) -> None:
+        pass
+    #handling government affiliation, only considered fully affiliated with .gov in domain
+    #it is 50/50 with .ac and .edu as some education facilities are private
+    def governmentAffiliation(domain):
+        suffix = tldextract.extract(domain).suffix
+        if suffix in Government(domain):
+            government_id = Government(id)
+        if 'edu' in suffix or 'ac' in suffix:
+            return governmentAffiliationResponse(50, government_id)
+        elif '.gov' in suffix:
+            return governmentAffiliationResponse(100, government_id)
+        else:
+            return governmentAffiliationResponse(0, government_id)
+
+#used to return two items for the governmentAffiliation method of attributeHandler object
+class governmentAffiliationResponse():
+    def __init__(self, levelOfAffiliation, government_id=None):
+        self.levelOfAffiliation = levelOfAffiliation 
+        self.government_id = government_id 
